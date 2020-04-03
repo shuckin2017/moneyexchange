eb3f953315f3d03a2609c0548ed7795c67064d22
shuckin2017 <shuckin@mail.ru>
2020-04-03 17:57:24 +0300

    created files

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,31 @@
 let gulp = require('gulp'), 
     sass = require('gulp-sass'),
-   browserSync = require('browser-sync').create();
+    browserSync = require('browser-sync').create();
+
+gulp.task('sass', function(){
+    return gulp.src('src/sass/**/*.sass')
+        .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
+        .pipe(gulp.dest('src/css'))
+        .pipe(browserSync.reload({stream: true}))
+});
+
+gulp.task('html', function(){
+    return gulp.src('src/*.html')
+        .pipe(browserSync.reload({stream: true}))
+});
+
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "src/"
+        }
+    });
+});
+
+
+gulp.task('watch', function(){
+    gulp.watch('src/sass/**/*.sass', gulp.parallel('sass'))
+    gulp.watch('src/*.html', gulp.parallel('html'))
+  });
+
+gulp.task('default', gulp.parallel('browser-sync', 'watch'))
